861ff1330615dab7732a39ac8f98f6e0b8f5812a
js4484 <js4484@columbia.edu>
2019-11-11 20:00:49 -0500

    last commit of day

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:password, :email, :first_name, :last_name, :photo, :billing,
-      :city, :state, :zipcode, :country, :shipping_city, :shipping_state, :shipping_zip, :shipping_country, :description)
+      :city, :state, :zipcode, :country, :shipping_city, :shipping_state, :shipping_zip, :shipping_country, :description,
+      :email_pref_1, :email_pref_2, :email_pref_3)
   end
 end
