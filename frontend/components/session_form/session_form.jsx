5c9e817173551fe3c6905108c3c1cbdd678bcbd0
js4484 <js4484@columbia.edu>
2019-11-13 17:50:59 -0500

    login signup workfow

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,15 +1,15 @@
+import { merge } from 'lodash';
 import React from 'react';
 
 class SessionForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
+            user: {email: '',
             password: '',
             first_name: '',
-            last_name: '',
+            last_name: ''},
             activeForm: 'greetingForm'
-
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -19,18 +19,26 @@ class SessionForm extends React.Component {
             [field]: e.currentTarget.value
         });
     }
+    updateUser(field) {
+        return e => this.setState(
+            merge({}, this.state, { user: { [field]: e.target.value } })
+        )
+    }
 
     handleSubmit(e) {
         e.preventDefault();
 
         if (this.state.activeForm === "greetingForm") {
-            // logic for login/signup
-            this.setState({ activeForm: "nextForm" })
+            // logic for login/signup; if email exists aF is login; else signup
+            this.setState({ activeForm: "##" })
         } else {
             const user = Object.assign({}, this.state);
             this.props.processForm(user);
         }
+    }
 
+    checkEmail(e) {
+        console.log("method that checks to confirm email is the one in state")
     }
 
     renderErrors() {
@@ -46,10 +54,10 @@ class SessionForm extends React.Component {
     }
 
     render() {
-        return (
+        const greetingForm = (
             <div className="login-form-container">
                 <form onSubmit={this.handleSubmit} className="login-form-box">
-                    <div class="greeting-with-img">
+                    <div className="greeting-with-img">
                         <div>Let's get Started</div>
                         <div>Use Facebook or email to get started.</div>
                     </div>
@@ -57,7 +65,7 @@ class SessionForm extends React.Component {
                     <div className="login-form">
                         <br />
                         <label>Email:
-                            <input type="text" value={this.state.email} onChange={this.update('email')} className="login-input" />
+                            <input type="text" value={this.state.user.email} onChange={this.updateUser('email')} className="login-input" />
                         </label>
                         <br />
                         <input className="session-submit" type="submit" value="Get Started" />
@@ -74,29 +82,85 @@ class SessionForm extends React.Component {
         const loginForm = (
             <div className="login-form-container">
                 <form onSubmit={this.handleSubmit} className="login-form-box">
-                    <div class="greeting-with-img">
+                    <div className="greeting-with-img">
                         <div>Welcome back</div>
                         <div>Please enter your password to log in.</div>
                     </div>
-                 <br />
-                    Please {this.props.formType} or {this.props.navLink}
                     {this.renderErrors()}
                     <div className="login-form">
                         <br />
                         <label>Email:
-                            <input type="text" value={this.state.email} onChange={this.update('email')} className="login-input"/>
+                            <input type="text" value={this.state.user.email} className="login-input" readonly />
                         </label>
                         <br />
                         <label>Password:
-                            <input type="password" value={this.state.password} onChange={this.update('password')} className="login-input"/>
+                            <input type="password" value={this.state.user.password} onChange={this.updateUser('password')} className="login-input"/>
                         </label>
                         <br />
-                        <input className="session-submit" type="submit" value="Get Started" />
+                        <input className="session-submit" type="submit" value="Log In" />
+                        <a href="#">Forgot password</a>
                     </div>
                 </form>
             </div>
         );
-    }
+
+        const signupForm = (
+            <div className="login-form-container">
+                <form onSubmit={this.handleSubmit} className="login-form-box">
+                    <div className="greeting-with-img">
+                        <div>Welcome</div>
+                        <div>Create an account.</div>
+                    </div>
+                    {this.renderErrors()}
+                    <div className="login-form">
+                        <br />
+                        <label>Email:
+                            <input type="text" value={this.state.user.email} className="login-input" readonly />
+                        </label>
+                        <br />
+                        <label>Confirm email:
+                            <input type="text" value={this.state.user.password} onChange={this.checkEmail} className="login-input" />
+                        </label>
+                        <br />
+                        <div className="signup-names">
+                            <label>First Name:
+                            <input type="text" value={this.state.user.first_name} onChange={this.updateUser('first_name')} className="login-input" />
+                            </label>
+                            <label>Last Name:
+                            <input type="text" value={this.state.user.last_name} onChange={this.updateUser('last_name')} className="login-input" />
+                            </label>
+                        </div>
+                        <br />
+                        <label>Password:
+                            <input type="password" value={this.state.user.password} onChange={this.updateUser('password')} className="login-input" />
+                        </label>
+                        <input className="session-submit" type="submit" value="Sign Up" />
+                        <div className="password-strength">
+                            <p>Your password must be at least 8 characters</p>
+                        </div>
+                        <a href="#">Log In Instead</a>
+                    </div>
+                </form>
+            </div>
+        )
+        let outputForm
+        switch (this.state.activeForm) {
+            case "greetingForm":
+                outputForm = greetingForm;
+                break
+            case "loginForm":
+                outputForm = loginForm;
+                break
+            case "signupForm":
+                outputForm = signupForm;
+                break
+        }
+        return (
+        <div>
+            {outputForm}
+        </div>
+        )
+    };
 }
 
 export default SessionForm;
