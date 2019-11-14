f64cd3e091d869013016be49aa01f9decb7208f5
js4484 <js4484@columbia.edu>
2019-11-14 16:28:42 -0500

    troubleshooting for heroku

--- db/migrate/20191111212121_create_users.rb
+++ db/migrate/20191111212121_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email, null: false, unique: true
+      t.string :email, null: false, index: true, unique: true
       t.string :first_name
       t.string :last_name
       t.string :photo
