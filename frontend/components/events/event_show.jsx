9b628b298e1e97213334c2582c9f09d515f1ce4a
js4484 <js4484@columbia.edu>
2019-11-18 16:22:40 -0500

    Sets up show page skeleton

--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -15,7 +15,33 @@ class EventShow extends React.Component {
             return null
         }
         const id = `Event Id: ${this.props.currentEvent.id}`
-        return (<div>{id}</div>)
+        return (<div className="show-page">
+            <div className="event-header">
+                <div className="event-header-image"></div>
+                <div className="event-header-details"></div>
+            </div>
+            <div className="evnt-midbar">
+                <div className="event-midbar-share"></div>
+                <div className="event-midbar-like"></div>
+                <div className="event-midbar-event-info"></div>
+                <div className="event-midbar-ticket-price"></div>
+                <div className="event-midbar-tickets"></div>
+            </div>
+            <div className="event-body">
+                <div className="event-body-description">
+                    <div className="event-body-description-title"></div>
+                    <div className="event-body-description-about">
+                        <h2>About this event</h2>
+                    </div>
+                    <div className="event-body-description-tags">Tags</div>
+                    <div className="event-body-description-share">Share</div>
+                </div>
+                <div className="event-body-sidebar"></div>
+
+            </div>
+        
+        
+        </div>)
     }
     
 }
