1ad66954082cd55972715737d46e20cd86c0a62f
js4484 <js4484@columbia.edu>
2019-11-18 19:54:27 -0500

    Sets up show page skeleton

--- app/views/api/events/show.json.jbuilder
+++ app/views/api/events/show.json.jbuilder
@@ -1,1 +1,5 @@
-json.extract! @event, :id, :event_title, :location
+json.extract! @event, :id, :event_title, :location, :meet_up_details, :event_date,
+    :start_time, :end_time, :event_image, :event_description, :add_faqs, :faqs,
+    :social_link_q, :social_links, :event_type, :event_topic, :tags, :ticket_num, 
+    :ticket_price, :ticket_country, :ticket_currency, :refund_policy, :private_event, 
+    :show_remaining_tickets, :organizer_id
--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -10,41 +10,94 @@ class EventShow extends React.Component {
         this.props.fetchEvent(this.props.match.params.eventId);
     }
 
+    parseDT() {
+        return ("parsed date and time")
+    }
+
+    parseHeaderDate() {
+        return ("header date")
+    }
+
+    parseHeaderPrice() {
+        return ("header price range")
+    }
+
+    parseHeaderTitle() {
+        return ("header title")
+    }
+
+
+    parseRefund() {
+        return ("parsed refund")
+    }
+    
+    parseTags() {
+        const tags = this.props.currentEvent.tags;
+        let tagsToo = Object.keys(tags);
+        let tagsOutput = [];
+        tagsToo.forEach((tag, i) => {
+            tagsOutput.push(<div className="event-tag-container"><li key={i} className="event-tag">{tag}</li></div>)
+            
+        })
+        return <ul>{tagsOutput}</ul>
+    }
+
+    parseTourguide() {
+        return ("parsed Tourguide")
+    }
+
     render() {
         if (!this.props.currentEvent) {
             return null
         }
         
-        const id = `Event Id: ${this.props.currentEvent.id}`
+        const pTg = `${this.parseTourguide()}`
+        const dt = `${this.props.currentEvent.start_time} - ${this.props.currentEvent.end_time}`
         // debugger;
         return (<div className="show-page">
             <div className="event-container">
                 <div className="event-header">
-                    <div className="event-header-image"></div>
-                    <div className="event-header-details"></div>
+                    <div className="event-header-image"><img src={this.props.currentEvent.event_image}/></div>
+                    <div className="event-header-details">
+                        <div className="event-header-date">{this.parseHeaderDate()}</div>
+                        <div className="event-header-title">{this.parseHeaderTitle()}</div>
+                        <div className="event-header-title">{this.parseHeaderPrice()}</div>
+                    </div>
                 </div>
                 <div className="event-midbar">
-                    <div className="event-midbar-share"></div>
-                    <div className="event-midbar-like"></div>
-                    <div className="event-midbar-event-info"></div>
-                    <div className="event-midbar-ticket-price"></div>
-                    <div className="event-midbar-tickets"></div>
+                    <div className="event-midbar-share midbar-share-icon"></div>
+                    <div className="event-midbar-like midbar-like-icon"></div>
+                    <div className="event-midbar-event-info">
+                        <div>{this.props.currentEvent.event_title}</div>
+                        <div>{this.props.currentEvent.event_date}</div>
+                    </div>
+                    <div className="event-midbar-ticket-price">{this.parseHeaderPrice()}</div>
+                    <div className="event-midbar-tickets"><div>Tickets</div></div>
                 </div>
                 <div className="event-body">
                     <div className="event-body-description">
                         <div className="event-body-description-title">{this.props.currentEvent.event_title}</div>
                         <div className="event-body-description-about">
                             <h2>About this event</h2>
+                            <p>{this.props.currentEvent.event_description}</p>
                         </div>
-                        <div className="event-body-description-tags">Tags</div>
+                        <div className="event-body-description-tags">{this.parseTags()}</div>
                         <div className="event-body-description-share">Share</div>
                     </div>
                     <div className="event-body-sidebar">
                         <div className="event-body-sidebar-details">
-                            <div className="event-body-sidebar-details-performers">Performers</div>
-                            <div className="event-body-sidebar-details-date">Date and Time</div>
-                            <div className="event-body-sidebar-details-location">Location</div>
-                            <div className="event-body-sidebar-details-refund">Refund Policy</div>
+                            <div className="event-body-sidebar-details-tourguide sidebar-element"><span>Tourguide</span>
+                                <div className="side-detail"><a href="#">{pTg}</a></div>
+                            </div>
+                            <div className="event-body-sidebar-details-date sidebar-element"><span>Date and Time</span>
+                                <div className="side-detail">{this.parseDT()}</div>
+                            </div>
+                            <div className="event-body-sidebar-details-location sidebar-element"><span>Location</span>
+                                <div className="side-detail">{this.props.currentEvent.location}</div>
+                            </div>
+                            <div className="event-body-sidebar-details-refund sidebar-element"><span>Refund Policy</span>
+                                <div className="side-detail">{this.parseRefund()}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
